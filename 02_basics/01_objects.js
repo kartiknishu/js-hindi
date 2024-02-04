7b5be564355f57c21d38600106cd15a2dabeb745
kartiknishu <120667199+kartiknishu@users.noreply.github.com>
2024-02-04 13:44:39 +0000

    init

---
+++ 02_basics/01_objects.js
@@ -0,0 +1,34 @@
+const sym = Symbol("mykey1");
+
+const jsuser = {
+    username: "Kartik",
+    email: "user@gmail.com",
+    [sym]: "key2",
+
+}
+
+// console.log(jsuser);
+
+// console.log(jsuser.username);
+
+// console.log(jsuser["username"]);
+
+// console.log(typeof jsuser[sym]);
+
+// jsuser.username = "nishu";
+
+// Object.freeze(jsuser);
+
+// jsuser.username = "kartik";
+
+// console.log(jsuser.username);
+
+// console.log('Hello Js user, ${jsuser.username}');
+
+const {username: name} = jsuser;
+
+console.log(name);
+
+
+
+
